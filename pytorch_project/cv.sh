a324d38358ad4f83147f6027b214b2e72192ec6d
gozian2811 <604914916@qq.com>
2020-06-17 11:38:03 +0800

    Create cv.sh

---
+++ pytorch_project/cv.sh
@@ -0,0 +1,22 @@
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold1\|10 --val_fold2 1
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold2\|10 --val_fold2 2
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold3\|10 --val_fold2 3
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold4\|10 --val_fold2 4
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold5\|10 --val_fold2 5
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold6\|10 --val_fold2 6
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold7\|10 --val_fold2 7
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold8\|10 --val_fold2 8
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold9\|10 --val_fold2 9
+#python main.py train --env BaseweightSharedBNNet_classification_scratch_adam_rfold10\|10 --val_fold2 10
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold4\|10 --val_fold2 4
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold5\|10 --val_fold2 5
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold6\|10 --val_fold2 6
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold7\|10 --val_fold2 7
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold8\|10 --val_fold2 8
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold9\|10 --val_fold2 9
+#python main.py train --env BaseweightSharedBNNet_bf_classification_adam_rfold10\|10 --val_fold2 10
+
+python main.py train --env DensecropNet_detection_2_rfold4 --val_fold 4
+python main.py train --env DensecropNet_detection_2_rfold5 --val_fold 5
+python main.py train --env DensecropNet_detection_2_rfold7 --val_fold 7
+python main.py train --env DensecropNet_detection_2_rfold10 --val_fold 10
